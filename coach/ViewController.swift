dfad0cd37bc37c53a5cc4d87d5e3f21edd7d98d7
André Antonio Faruolo Filho <andre_antonio1900@hotmail.com>
2017-12-28 18:31:09 -0200

    first commit

---
+++ coach/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coach
+//
+//  Created by Andre Faruolo on 28/12/2017.
+//  Copyright © 2017 com.br.andre.faruolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
